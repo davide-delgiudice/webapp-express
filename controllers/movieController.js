d0a637e440556435f95793cf055ad72eb4c6b867
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-29 18:50:23 +0200

    [UPD]  made second show query for showing reviews

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,10 +2,10 @@ const connection = require('../data/db');
 
 // metodo index del controller
 const index = (req, res) => {
-    connection.query("SELECT * FROM movies", (err, moviesResult) => {
+    connection.query("SELECT * FROM movies", (err, results) => {
         if(err) return res.status(500).json({error: "Database query failed: "+err});
       
-        res.json(moviesResult);
+        res.json(results);
     })
 };
 
@@ -19,18 +19,38 @@ const show = (req, res) => {
         SELECT *
         FROM movies
         WHERE id = ?
-    `
+    `;
+
+    // query per recuperare le recensioni del film selezionato
+    const reviewSql =`
+        SELECT *
+        FROM reviews
+        WHERE movie_id = ?
+    `;
 
     // eseguo la prima query
     connection.query(movieSql, [id], (err, moviesResult) => {
         if(err) {
-            return res.status(500).json({error: "Database query failed: "+err});
-        }
+            return res.status(500).json({error: "Database query failed: "+err})
+        };
 
         // controllo se il film esiste
         if(moviesResult.length === 0) return res.status(404).json({error: "Movie not found"});
-    })
 
+        // recupero il film in posizione 0
+        const movie = moviesResult[0];
+
+        // eseguo la seconda query
+        connection.query(reviewSql, [id], (err, reviewResult) => {
+            if(err) {
+                return res.status(500).json({error: "Database query failed: "+err})
+            };
+
+            movie.reviews = reviewResult;
+
+            res.json(movie);
+        });
+    });
 };
 
 
