533c2e0c3e7b8d8dea712f8f33daacfccdd93965
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-29 16:06:11 +0200

    [UPD[ import express, add port and base root

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("We")
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Benvenuto su Movies Express!")
+});
+
+app.listen(port, () => {
+    console.log(`Server in ascolto sulla porta ${port}`)
+});
