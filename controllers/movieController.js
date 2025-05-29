47dec611cb53fad4679dcd77c4e97aef97e25c61
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-29 18:18:55 +0200

    [UPD] made show query and condition for check movie existence

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -12,8 +12,27 @@ const index = (req, res) => {
 
 // metodo show del controller
 const show = (req, res) => {
-    console.log('la mia show')
+    const id = req.params.id;
+
+    // query per recuperare il film con il rispettivo ID
+    const movieSql = `
+        SELECT *
+        FROM movies
+        WHERE id = ?
+    `
+
+    // eseguo la prima query
+    connection.query(movieSql, [id], (err, moviesResult) => {
+        if(err) {
+            return res.status(500).json({error: "Database query failed: "+err});
+        }
+
+        // controllo se il film esiste
+        if(moviesResult.length === 0) return res.status(404).json({error: "Movie not found"});
+    })
+
 };
+
 
 module.exports = {
     index, show
