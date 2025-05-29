a26b74b37fe80ef8c9ffad1fb4252b762098359c
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-29 17:11:51 +0200

    [UPD] create root for index and show, and exported router

--- routers/movie.js
+++ routers/movie.js
@@ -3,3 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const movieController = require('../controllers/movieController');
+
+// rotta index
+router.get('/', movieController.index);
+
+// rotta show
+router.get('/:id', movieController.show);
+
+
+module.export = router;
